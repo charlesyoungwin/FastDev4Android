7096a66cbd5c5e29276a21fb110421f6b5bee5e0
杨壮 <zhuang.yang@hunteron.com>
2017-04-17 17:54:45 +0800

    start

---
+++ app/src/main/java/com/chinaztt/fda/test/PullListviewActivityPrac.java
@@ -0,0 +1,19 @@
+package com.chinaztt.fda.test;
+
+import android.os.Bundle;
+
+import com.chinaztt.fda.ui.R;
+import com.chinaztt.fda.ui.base.BaseActivity;
+
+/**
+ * Created by charl on 2017/4/17.
+ */
+
+public class PullListviewActivityPrac extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pull_listview_layout);
+    }
+}
---
+++ app/src/main/java/com/chinaztt/fda/widget/PullToRefreshListViewPrac.java
@@ -0,0 +1,15 @@
+package com.chinaztt.fda.widget;
+
+import android.widget.ListView;
+
+/**
+ * Created by charl on 2017/4/17.
+ */
+
+public class PullToRefreshListViewPrac extends ListView {
+
+    public PullToRefreshListViewPrac(){
+        super();
+    }
+
+}
